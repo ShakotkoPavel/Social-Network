361f2fc7c1e2ea54c2f33fa537c8c953cd22a519
Pavlo Shakotko <pavlo.sh@sbtech.com>
2020-04-22 21:09:06 +0300

    added new components

---
+++ src/components/Profile.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Profile = () => {
+  return (
+    <div className="content">
+      Main content
+      <div>ava + description</div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Profile;
